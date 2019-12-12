23cdd1c83081ff189ce9a0ab5233582d557392f1
Akyush <aayushkhandelwal4@gmail.com>
2019-12-12 21:08:49 +0530

    Updates

---
+++ precache-manifest.c3e5f47ea4dd2ea3936c6c492dc494ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb793b6a3a3e8024d3a8",
+    "url": "/About-Aayush-Khandelwal/static/css/main.89596927.chunk.css"
+  },
+  {
+    "revision": "cb793b6a3a3e8024d3a8",
+    "url": "/About-Aayush-Khandelwal/static/js/main.cb793b6a.chunk.js"
+  },
+  {
+    "revision": "4dbc9a6c5267740a0bb0",
+    "url": "/About-Aayush-Khandelwal/static/js/runtime~main.4dbc9a6c.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/About-Aayush-Khandelwal/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e1fd7cc451a14764d80ae19ec061a3ee",
+    "url": "/About-Aayush-Khandelwal/index.html"
+  }
+];
