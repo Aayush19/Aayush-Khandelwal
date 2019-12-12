39ea706b1857be4658f9f74fabb7b25d12366982
Akyush <aayushkhandelwal4@gmail.com>
2019-12-12 21:03:54 +0530

    Initial

---
+++ src/components/introduction.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+// import '../../public/css/image.css'
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li
+                style={{
+                  backgroundImage: "url(images/img_bg.jpg)",
+                  Height: "600px",
+                  transform: [{ rotate: "90deg" }]
+                }}
+              >
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1 style={{ color: "white" }}>
+                            Hi! <br />
+                            I'm Aayush`
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://drive.google.com/open?id=19iiTwEFL5GMER94252HKCgFY9bQxN52a"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              style={{ color: "white" }}
+                            >
+                              View CV
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1 style={{ color: "white" }}>
+                            I love building
+                            <br /> THINGS !!
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://github.com/Aayush19"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              style={{ color: "white" }}
+                            >
+                              View Projects <i className="icon-briefcase3" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              {/* <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I often <br/>Write ... </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li> */}
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Internship at ThunderPod Pvt. Ltd.{" "}
+                          <span>June/2019 - July/2019</span>
+                        </h2>
+                        <p>
+                          I recently have completed my internship at ThunderPod
+                          Pvt. Ltd. as a Software Developer-Intern in the DevOps
+                          team. My major part of the work has been into the
+                          field of creating Multi-Admin Login Panel for the
+                          Company. I explored various libraries of ReactJS and
+                          Node.js also explored other technologies like
+                          expressJS and Sqlite3.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Undergraduation at SVVV<span>2015-2019</span>
+                        </h2>
+                        <p>
+                          I am pursuing my under-graduation studies with major
+                          in C.C.E.(Computer & Communication Engineering). I
+                          have taken courses like DSA, OOPs, Computer Networks,
+                          over the years and have better understanding of these
+                          subjects.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Primary and Higher Education <span>2003-2016</span>
+                        </h2>
+                        <p>
+                          I have completed my higher secondary education with
+                          major subjects as Physics,Chemistry & Maths. During my
+                          time at school, I have developed interest in Computer
+                          field and started working on the coding platform,
+                          since then i have my keen interest in Computer Science
+                          field.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
